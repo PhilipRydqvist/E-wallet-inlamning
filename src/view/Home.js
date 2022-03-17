873184f1370456dd9c7bfcd0de4cc174fb3291e7
Philip Rydqvist <philip.rydqvist@gmail.com>
2022-03-17 11:39:24 +0100

    commit

---
+++ src/view/Home.js
@@ -0,0 +1,96 @@
+// import { Link } from 'react-router-dom';
+import styled from "styled-components";
+
+import { useNavigate } from "react-router-dom";
+
+import Top from "../components/Top";
+import Card from "../components/Card";
+import CardStack from "../components/CardStack";
+import DeleteButton from "../components/DeleteButton";
+
+function Home({creditCards, setCreditCards, activeCreditCard, setActiveCreditCard}) {
+    console.log(activeCreditCard);
+   
+    let navigate = useNavigate(); 
+
+    // <h1>detta är homesidan.</h1>
+    return (
+        
+        <HomeWrapper> 
+            <div className="headline-container">
+            <Top headline="E-WALLET" />
+            </div>
+
+            <p className="header">ACTIVE CARD</p>
+
+            <div className="activecard-container">
+            { activeCreditCard.length >= 1 ? <DeleteButton creditCards={ creditCards } setCreditCards={ setCreditCards } activeCreditCard= { activeCreditCard } setActiveCreditCard= { setActiveCreditCard }/> : null }
+            {activeCreditCard.map((cards) => (
+              <Card key={cards.id}
+              {...cards}creditCards={ creditCards } setCreditCards={ setCreditCards } activeCreditCard= { activeCreditCard } setActiveCreditCard= { setActiveCreditCard }
+              // spread operator ...
+              />  
+            ))}
+            </div>
+            <div className="cardstack-container">
+            <CardStack creditCards={ creditCards } setCreditCards={ setCreditCards } activeCreditCard= { activeCreditCard } setActiveCreditCard= { setActiveCreditCard }/>
+              {/* propdrillar */}
+            </div>
+            <button className="addCardButton" onClick={() => navigate("/add")}>ADD A NEW CARD</button>
+        </HomeWrapper>
+        
+
+        
+    );
+}
+
+const HomeWrapper = styled.section`
+  /*  min-height: 80vh;
+  min-width: 100%; 
+  background-color: white;
+   display: flex; 
+   justify-content: center; 
+  align-items: center;
+  flex-direction: column; */
+
+  
+   
+  .addCardButton {
+  position: absolute;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  } 
+  
+.header {
+  display: flex;
+  justify-content: center;
+  flex-direction: column; 
+  align-items: center;
+}
+
+.activecard-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column; 
+  align-items: center;
+}
+
+.cardstack-container {
+  display: flex;
+  position: relative;
+  flex-direction: column; 
+  align-items: center;
+}
+
+`
+
+export default Home;
+
+//  <section>
+        //  <button onClick= { Link to="add" }></button>
+        //  </section>
+        
+        //  <button
+        //       <Link to="/add">AddCard</Link>
+        //  />
